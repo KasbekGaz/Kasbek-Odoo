425418244fcd8adfe4cbf64700b8e25392b2cd65
KasbekGaz <oswa.laza.flores@hotmail.com>
2025-05-08 20:50:55 -0600

    Mi primer modulo

---
+++ estambre/__manifest__.py
@@ -0,0 +1,13 @@
+{
+'name': 'Estambre',
+'summary': "Estambre es un modulo de pruebas propiedad de Lázaro.",
+'version': '1.0',
+'category': 'Uncategorized',
+'author': 'Lázaro',
+'website': '',
+'depends': ['base', 'web'],
+'data': [],
+'application': False,
+'installable': True,
+'auto_install': False,
+}
