8b9ad6799954081566489e2ef2e55eaab4b2cc2d
KasbekGaz <oswa.laza.flores@hotmail.com>
2025-11-02 01:21:42 -0600

    [CHANGE] Se cambia el nombre del módulo y se agregan las rutas del archivo de seguridad y vistas

--- biblioteca/__manifest__.py
+++ biblioteca/__manifest__.py
@@ -1,12 +1,18 @@
 {
-'name': 'Bibliioteca',
+'name': 'Biblioteca',
 'summary': "Un módulo para gestionar libros y autores.",
 'version': '1.0',
 'category': 'Uncategorized',
 'author': 'Lázaro',
 'website': '',
 'depends': ['base', 'web'],
-'data': [],
+'data': [
+    # Seguridad
+    'security/ir.model.access.csv',
+    # Vistas
+    'views/libro_views.xml',
+],
+'demo': [],
 'application': True,
 'installable': True,
 'auto_install': False,
